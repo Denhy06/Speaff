ce35bb1ea25c37d5073615479c47d2c82a9c0018
Denhy06 <denimhy2@gmail.com>
2026-08-14 19:13:00 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+};
 
 export default nextConfig;
